a79ccb0aebcd9a70cf12324c5f16c6225aeafa33
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 20:28:46 +0200

    Add GetProductsQuery and handler for product retrieval
    Introduced GetProductsQuery and its handler using MediatR to fetch all products via IProductService and map them to ProductResponse DTOs. Results are returned in a standardized ResponseWrapper. Also removed a redundant folder inclusion from Application.csproj.

---
+++ Application/Features/Products/Queries/GetProductsQuery.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+
+using AutoMapper;
+
+using Common.Responses.Products;
+using Common.Wrappers;
+
+using MediatR;
+
+namespace Application.Features.Products.Queries;
+
+public class GetProductsQuery : IRequest<ResponseWrapper<IEnumerable<ProductResponse>>>
+{
+}
+
+public class GetProductsQueryHandler(IProductService productService, IMapper mapper) 
+    : IRequestHandler<GetProductsQuery, ResponseWrapper<IEnumerable<ProductResponse>>> 
+{ 
+    public async Task<ResponseWrapper<IEnumerable<ProductResponse>>> Handle(GetProductsQuery request, CancellationToken cancellationToken) 
+    { 
+        var products = await productService.GetAllAsync(cancellationToken); 
+        return new ResponseWrapper<IEnumerable<ProductResponse>>().Success(mapper.Map<IEnumerable<ProductResponse>>(products)); 
+    } 
+}
