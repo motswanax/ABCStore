37837bf2cffb2de08acb83d64597e23da08ed2b4
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 18:08:43 +0200

    Add MediatR & AutoMapper setup to Application project
    Added NuGet references for MediatR and AutoMapper in Application.csproj. Introduced ServiceCollectionExtensions.cs with AddApplicationServices extension method to register MediatR and AutoMapper using the current assembly for dependency injection.

---
+++ Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System.Reflection;
+
+namespace Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly(); // Get the current assembly
+        return services
+            .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly))
+            .AddAutoMapper(cfg => cfg.AddMaps(assembly));
+    }
+}
