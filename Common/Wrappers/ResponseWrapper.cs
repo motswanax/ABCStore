0b780290bcd3a8ee2047ea4108783f8d2f838c98
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 18:17:01 +0200

    Add generic ResponseWrapper class for standardized responses
    Introduced ResponseWrapper<T> in Common.Wrappers to encapsulate operation results with data, success status, and message. Includes Success and Fail methods for consistent response handling.

---
+++ Common/Wrappers/ResponseWrapper.cs
@@ -0,0 +1,23 @@
+﻿namespace Common.Wrappers;
+
+public class ResponseWrapper<T>
+{
+    public T Data { get; set; } = default!;
+    public bool IsSuccessful { get; set; }
+    public string? Message { get; set; }
+
+    public ResponseWrapper<T> Success(T data, string? message = null)
+    {
+        IsSuccessful = true;
+        Message = message;
+        Data = data;
+        return this;
+    }
+
+    public ResponseWrapper<T> Fail(string message)
+    {
+        IsSuccessful = false;
+        Message = message;
+        return this;
+    }
+}
