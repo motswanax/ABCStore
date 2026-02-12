cc60360d20fe74842ca89c9788280f5b775fda3d
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 17:50:55 +0200

    Add product request models and update project folder structure
    Added CreateProductRequest and UpdateProductRequest classes for product operations under Common.Requests.Products. Updated Common.csproj to include a new Responses/Products folder.

---
+++ Common/Requests/Products/CreateProductRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Requests.Products;
+
+public class CreateProductRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+}
---
+++ Common/Requests/Products/UpdateProductRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Requests.Products;
+
+public class UpdateProductRequest : CreateProductRequest
+{
+        public int Id { get; set; }
+}
