1c8a2205fe8ff6c6d5f44e0cdd54b1b3eb7838be
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 17:53:26 +0200

    Update Category model and add category request DTOs
    - Make Category.Description non-nullable with a default value
    - Add CreateCategoryRequest and UpdateCategoryRequest DTOs
    - Organize new DTOs under Common.Requests.Categories namespace

---
+++ Common/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace Common.Requests.Categories;
+
+public class UpdateCategoryRequest : CreateCategoryRequest
+{
+    public int Id { get; set; }
+}
--- Domain/Category.cs
+++ Domain/Category.cs
@@ -4,7 +4,7 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     // Navigation property
     public IEnumerable<Product> Products { get; set; } = [];
