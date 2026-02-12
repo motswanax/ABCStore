9d5dd88076b8a1ae350778c6873a69f82e2fc989
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 17:57:27 +0200

    Add CategoryResponse and ProductResponse classes
    Added new response classes for categories and products with relevant properties. Also removed unused folder reference from Common.csproj.

---
+++ Common/Responses/Categories/CategoryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Responses.Categories;
+
+public class CategoryResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
---
+++ Common/Responses/Products/ProductResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Responses.Products;
+
+public class ProductResponse
+{
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public string Description { get; set; } = null!;
+}
