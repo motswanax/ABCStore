8979bb36928f8c6c7dca934b0d5c01857497ffb4
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 20:46:23 +0200

    Add EF Core dependencies and ApplicationDbContext class
    Added EF Core packages to Infrastructure and WebApi projects.
    Created ApplicationDbContext with DbSets for Category and Product.

---
+++ Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Product> Products => Set<Product>();
+}
