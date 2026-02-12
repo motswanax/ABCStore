32e062b7465e1878eca6218459b30484a4748728
Baiketli Dijo <baiketlid@gmail.com>
2026-02-12 20:36:52 +0200

    Add query and handler for products by category ID
    Introduced GetProductsByCategoryIdQuery and handler using MediatR, IProductService, and AutoMapper to fetch and map products for a given category. Returns appropriate success or failure responses based on results.

---
+++ Application/Features/Products/Queries/GetProductsByCategoryIdQuery.cs
@@ -0,0 +1,31 @@
+﻿using Application.Services;
+
+using AutoMapper;
+
+using Common.Responses.Products;
+using Common.Wrappers;
+
+using MediatR;
+
+namespace Application.Features.Products.Queries;
+
+public class GetProductsByCategoryIdQuery : IRequest<ResponseWrapper<IEnumerable<ProductResponse>>>
+{
+    public int CategoryId { get; set; }
+}
+
+public class GetProductsByCategoryIdQueryHandler(IProductService productService, IMapper mapper) : IRequestHandler<GetProductsByCategoryIdQuery, ResponseWrapper<IEnumerable<ProductResponse>>>
+{
+    public async Task<ResponseWrapper<IEnumerable<ProductResponse>>> Handle(GetProductsByCategoryIdQuery request, CancellationToken cancellationToken)
+    {
+        var products = await productService.GetProductsByCategoryIdAsync(request.CategoryId, cancellationToken);
+
+        if (products.Count() == 0)
+        {
+            return new ResponseWrapper<IEnumerable<ProductResponse>>().Fail("No products found for the specified category.");
+        }
+
+        var productResponses = mapper.Map<IEnumerable<ProductResponse>>(products);
+        return new ResponseWrapper<IEnumerable<ProductResponse>>().Success(productResponses);
+    }
+}
