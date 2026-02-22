359e0cee9b70a71de72cfdd116918503b8314179
Baiketli Dijo <baiketlid@gmail.com>
2026-02-22 21:42:26 +0200

    Seed test DB from JSON in ProductServiceIntegrationTests
    Added logic to read and deserialize seeddata.json for test DB
    initialization. Used Shouldly for null checks and included
    necessary using statements for Shouldly, System.Text.Json,
    and DataInitializer.

--- Infrastructure.IntegrationTests/ProductServiceIntegrationTests.cs
+++ Infrastructure.IntegrationTests/ProductServiceIntegrationTests.cs
@@ -1,9 +1,14 @@
 ﻿using Application.Services;
 
 using Infrastructure.Contexts;
+using Infrastructure.IntegrationTests.Data;
 using Infrastructure.Services;
 
 using Microsoft.EntityFrameworkCore;
+
+using Shouldly;
+
+using System.Text.Json;
 
 namespace Infrastructure.IntegrationTests;
 
@@ -20,6 +25,20 @@ public class ProductServiceIntegrationTests : IDisposable
         _context = new ApplicationDbContext(options);
 
         _productService = new ProductService(_context);
+
+        // Seed database
+        var initializingDataJson = File.ReadAllText("Data\\seeddata.json");
+        var initializingData = JsonSerializer.Deserialize<DataInitializer>(
+            initializingDataJson,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+        initializingData.ShouldNotBeNull();
+        initializingData.Categories.ShouldNotBeNull();
+        initializingData.Products.ShouldNotBeNull();
+
+        _context.Categories.AddRange(initializingData.Categories);
+        _context.Products.AddRange(initializingData.Products);
+        _context.SaveChanges(); 
     }
 
     public void Dispose()
